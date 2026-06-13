4dfbb514a6c1459a13a63d53955506fc8d7523c6
GabrielFGuerreiro <gaferg2004@gmail.com>
2026-06-12 21:35:17 -0300

    Iniciadas validações de campos e senha no cadastro.

--- cadastrar.php
+++ cadastrar.php
@@ -1,41 +1,11 @@
 <?php 
-    include "header.php";
-    include "Models/Database.php";
-?>
+include "header.php";
+include "Models/Database.php";
 
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastrar</title>
-</head>
-<body>
-    <form action="" method="POST">
-        <label for="nome">Nome</label>
-        <input type="text" id="nome" name="nome">
+if (isset($_GET['sucesso'])) {
+    echo "Cadastro realizado com sucesso!";
+}
 
-        <label for="cpf">CPF</label>
-        <input type="text" id="cpf" name="cpf">
-
-        <label for="dtNasc">Data de Nascimento</label>
-        <input type="date" id="dtNasc" name="dtNasc">
-
-        <label for="cel">Celular</label>
-        <input type="text" id="cel" name="cel">
-
-        <label for="emailCad">E-mail</label>
-        <input type="email" id="emailCad" name="emailCad">
-
-        <label for="senhaCad">Senha</label>
-        <input type="password" id="senhaCad" name="senhaCad">
-
-        <button type="submit">Cadastrar</button>
-    </form>
-</body>
-</html>
-
-<?php
 if($_SERVER['REQUEST_METHOD'] === 'POST')
 {
     $nome = $_POST['nome'];
@@ -57,21 +27,23 @@ if($_SERVER['REQUEST_METHOD'] === 'POST')
 
         if ($count > 0) {
             echo "CPF Já Cadastrado.";
-            exit;
         }
-        
-        $sql = "INSERT INTO USUARIOS (nmUsuario, cpf, dataNasc, email, celular, isAdmin, senha) 
+        else
+        {            
+            $sql = "INSERT INTO USUARIOS (nmUsuario, cpf, dataNasc, email, celular, isAdmin, senha) 
                 VALUES (:nome, :cpf, :dataNasc, :email, :celular, 0, :senha)";
-        $stmt = $pdo->prepare($sql);
-        $stmt->execute([
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute([
             ":nome" => $nome,
             ":cpf" => $cpf,
             ":dataNasc" => $dtNasc,
             ":email" => $email,
             ":celular" => $cel,
             ":senha" => $senha            
-        ]);
-        echo "Cadastro Realizado com Sucesso!";
+            ]);
+            header("Location: cadastrar.php?sucesso=1"); //Quando cadastrar, não fica na requisição POST, vai para GET, impedindo o reenvio do form quando recarregar.
+            exit();
+        }
     }
     catch(PDOException $e)
     {
@@ -80,3 +52,118 @@ if($_SERVER['REQUEST_METHOD'] === 'POST')
 }
 
 ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastrar</title>
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+</head>
+<body>
+    <form action="" method="POST" id="formCadastrar">
+        <label for="nome">Nome</label>
+        <input type="text" id="nome" name="nome">
+        <spam id="msgObrigatoriaNome" class="msgObrigatoria">O <b>Nome</b> é Obrigatório.</spam>
+
+        <label for="cpf">CPF</label>
+        <input type="text" id="cpf" name="cpf">
+        <spam id="msgObrigatoriaCpf" class="msgObrigatoria">O <b>CPF</b> é Obrigatório.</spam>
+
+        <label for="dtNasc">Data de Nascimento</label>
+        <input type="date" id="dtNasc" name="dtNasc">
+
+        <label for="cel">Celular</label>
+        <input type="text" id="cel" name="cel">
+
+        <label for="emailCad">E-mail</label>
+        <input type="email" id="emailCad" name="emailCad">
+        <spam id="msgObrigatoriaEmail" class="msgObrigatoria">O <b>CPF</b> é Obrigatório.</spam>
+
+        <label for="senhaCad">Senha</label>
+        <input type="password" id="senhaCad" name="senhaCad">
+        <spam id="msgObrigatoriaSenha" class="msgObrigatoria">A <b>Senha</b> é Obrigatória.</spam>
+
+        <spam class="requisitosSenha"><i class="fa fa-circle"></i>8 Caracteres</spam>  
+        <spam class="requisitosSenha"><i class="fa fa-circle"></i>1 Letra Minúscula</spam>  
+        <spam class="requisitosSenha"><i class="fa fa-circle"></i>1 Letra Maiúscula</spam>  
+        <spam class="requisitosSenha"><i class="fa fa-circle"></i>1 Número</spam>  
+        <spam class="requisitosSenha"><i class="fa fa-circle"></i>1 Caracter Especial</spam>  
+
+        <button type="button" id="btnCadastrar">Cadastrar</button>
+    </form>
+</body>
+</html>
+
+
+<style>
+    .msgObrigatoria {
+        display: none;
+        color:red;
+    }
+
+    .requisitosSenha {
+        font-size: 10px;
+    }
+
+    .fa-circle {
+        color:gray;
+        font-size: 10px;
+        margin-right: 5px;
+    }
+</style>
+
+
+<script>
+    document.getElementById("btnCadastrar").addEventListener("click", function()
+    {
+        var icCadastrar = true;
+        // var nome = document.getElementById("nome").value;
+        // if(!nome)
+        // {
+        //     document.getElementById("msgObrigatoriaNome").style.display = "block";
+        //     icCadastrar = false;
+        // }
+
+        // var cpf = document.getElementById("cpf").value;
+        // if(!cpf)
+        // {
+        //     document.getElementById("msgObrigatoriaCpf").style.display = "block";
+        //     icCadastrar = false;
+        // }
+
+        
+        var senhaCad = document.getElementById("senhaCad").value;
+        if(!senhaCad)
+        {
+            document.getElementById("msgObrigatoriaSenha").style.display = "block";
+            icCadastrar = false;
+        }
+
+        //if(icCadastrar == true)
+        if(1==0)
+        {
+            document.getElementById("formCadastrar").submit();
+        }
+        
+        document.querySelectorAll(".fa-circle").forEach((element) => {
+            element.style.color = "red";
+        });
+        
+    });
+
+
+    document.getElementById("senhaCad").addEventListener("input", function()
+    {
+        console.log(this.value);
+        var senha = this.value;
+        var circulos = document.getElementsByClassName("fa-circle");
+
+        circulos[0].style.color = senha.length >= 8 ? "red" : "gray";
+
+        
+        circulos[1].style.color = senha.match(/\w/g) ? "red" : "gray";
+        circulos[3].style.color = senha.match(/[a-z]/g) ? "red" : "gray";
+
+    });
+</script>
