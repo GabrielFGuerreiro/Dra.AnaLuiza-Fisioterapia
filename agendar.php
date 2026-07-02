4a79cdc88353f79e164cd48f6562026a118e6aec
megatails <megatails03@gmail.com>
2026-07-02 20:35:39 -0300

    limpeza no .css, adição e atualização das telas de agendamento

---
+++ agendar.php
@@ -0,0 +1,30 @@
+<?php 
+include "header.php";
+require_once "Models/Database.php";
+require_once "scripts/calendario-lib.html";
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agendar</title>
+</head>
+<body>
+    <div class="conteudo">
+        <h2>Lista de Agendamentos</h2>
+        <div class="card" id='calendar' style="width: 400px; height: 550px;"></div>
+        <script >
+            const { Calendar } = calendarjs;
+
+            Calendar(document.getElementById('calendar'), {
+                type: 'inline',
+                time: true,
+                grid: true,
+                value: new Date()
+            });
+        </script>
+    </div>
+</body>
+</html>
