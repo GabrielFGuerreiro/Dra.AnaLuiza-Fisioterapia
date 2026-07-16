86f3be88a6a78070617169756e5a8056eee2a472
GabrielFGuerreiro <gaferg2004@gmail.com>
2026-07-16 06:08:14 -0300

    retirada vendor do gitignore

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'gfg/draanaluiza',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '81bd3a0c4267b8dd238ec4aed78f2fb29aee80fa',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'gfg/draanaluiza' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '81bd3a0c4267b8dd238ec4aed78f2fb29aee80fa',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
