e59a00b9799154b1d5580e46b099012e0ead4f50
GabrielFGuerreiro <gaferg2004@gmail.com>
2026-07-18 18:54:23 -0300

    Retirada importação desnecessária do style.

--- Views/agendamentos.php
+++ Views/agendamentos.php
@@ -8,7 +8,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles\custom-style.css">
     <title>Agendamentos</title>
 </head>
 <body>
