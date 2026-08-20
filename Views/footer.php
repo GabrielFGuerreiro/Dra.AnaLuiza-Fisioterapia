08f9d0af8efebb34b4a8097debb013ff6b704fda
megatails <megatails03@gmail.com>
2026-08-20 20:37:38 -0300

    arrumado o display do mapa no footer

--- Views/footer.php
+++ Views/footer.php
@@ -19,12 +19,14 @@
                 </a>
             </div>
         </div>
+        <div class="row mt-3" style>
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14578.671613376457!2d-46.42367727447356!3d-24.007501555852826!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce1d8ae5e159f3%3A0x4e9e86bb3d408475!2sFisioterapia%20%7C%20Dra.%20Ana%20Luiza%20da%20Silva!5e0!3m2!1spt-BR!2sbr!4v1787095495393!5m2!1spt-BR!2sbr"
+                style="border:0;"
+                allowfullscreen
+                loading="lazy"
+                referrerpolicy="strict-origin-when-cross-origin">
+            </iframe>
+        </div>
     </div>
-    <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14578.671613376457!2d-46.42367727447356!3d-24.007501555852826!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce1d8ae5e159f3%3A0x4e9e86bb3d408475!2sFisioterapia%20%7C%20Dra.%20Ana%20Luiza%20da%20Silva!5e0!3m2!1spt-BR!2sbr!4v1787095495393!5m2!1spt-BR!2sbr"
-        style="border:0;"
-        allowfullscreen
-        loading="lazy"
-        referrerpolicy="strict-origin-when-cross-origin">
-    </iframe>
 </footer>
